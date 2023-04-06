e0e100d3c7d39f916be4f58c0f5ba7e9bf8e79c2
kavan4321 <kavanpatel432001@gmail.com>
2023-04-06 17:39:22 +0530

    change

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,6 +3,8 @@ using Chapter8.View.Exercise1;
 using Chapter8.View.Exercise2;
 using Chapter8.View.Exercise3;
 using Chapter8.View.Exercise4;
+using Chapter8.View.Exercise5;
+using Chapter8.View.Exercise6;
 
 public partial class App : Application
 {
@@ -10,6 +12,7 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new NavigationPage(new ChatScreen());
+		MainPage = new NavigationPage(new LoginScreen())
+		{ BarBackgroundColor = Colors.DarkCyan };
 	}
 }
--- View/Exercise6/LoginScreen.xaml.cs
+++ View/Exercise6/LoginScreen.xaml.cs
@@ -1,3 +1,5 @@
+using CommunityToolkit.Maui.Alerts;
+
 namespace Chapter8.View.Exercise6;
 
 public partial class LoginScreen : ContentPage
@@ -6,4 +8,79 @@ public partial class LoginScreen : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void UserNameChange()
+    {
+        Resources["UsernameBorder"] = Resources["UsernameBorderInvalid"];
+        Resources["UsernameText"] = Resources["UsernameTextInvalid"];
+    }
+
+    private void PasswordChange()
+    {
+        Resources["PasswordBorder"] = Resources["PasswordBorderInvalid"];
+        Resources["PasswordText"] = Resources["PasswordTextInvalid"];
+    }
+
+	private void Validation()
+	{
+        var userName = UserNameEntry.Text;
+        var password = PasswordEntry.Text;
+
+        if (string.IsNullOrWhiteSpace(userName) && string.IsNullOrWhiteSpace(password))
+        {
+            UserNameChange();
+            PasswordChange();
+        }
+        else if (string.IsNullOrWhiteSpace(userName) || userName != "john")
+        {
+            UserNameChange();
+        }
+
+        else if (string.IsNullOrWhiteSpace(password) || password.Length<4)
+        {
+            PasswordChange();
+        }
+
+        else
+        {
+            Toast.Make("Login Successful", CommunityToolkit.Maui.Core.ToastDuration.Short).Show();
+        }
+    }
+
+    private void LoginButtonClicked(object sender, EventArgs e)
+    {
+        Validation();
+    }
+
+
+    private void UserNameEntryTextChanged(object sender, TextChangedEventArgs e)
+    {
+        var userName = UserNameEntry.Text;
+
+        if (string.IsNullOrWhiteSpace(userName) || userName != "john")
+        {
+            UserNameChange();
+        }
+        else
+        {
+            Resources["UsernameBorder"] = Resources["UsernameBorder"];
+            Resources["UsernameText"] = Resources["UsernameText"];
+        }
+    }
+
+
+    private void PasswordEntryTextChanged(object sender, TextChangedEventArgs e)
+    {
+        var password = PasswordEntry.Text;
+
+       if (string.IsNullOrWhiteSpace(password) || password.Length < 4)
+        {
+            PasswordChange();
+        }
+        else
+        {
+            Resources["PasswordBorder"] = Resources["PasswordBorder"];
+            Resources["PasswordText"] = Resources["PasswordText"];
+        }
+    }
 }
